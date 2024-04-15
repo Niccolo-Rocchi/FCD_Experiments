ed45e9bdceb1df72aca204aefb20a68c30b0c78e
Niccolo-Rocchi <nik9991u@gmail.com>
2024-04-15 17:12:30 +0200

    Fix venv activation

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Instructions
 
-1. To generate data, type `Rscript data_generation.r`.
-2. To run NOTEARS-LINEAR-ADMM, type `source venv/bin/activate` and then `python notears-admm.py`
-3. To run all, type `bash run.sh`
+1. To run all, type `bash run.sh`
--- run.sh
+++ run.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/bash
 
-# Download source code
+# Download code
 git clone https://github.com/ignavierng/notears-admm.git
 
 # Generate data 
 Rscript data_generation.r
 
-# Run code
+# Activate venv
+python -m venv venv
 source venv/bin/activate
+pip install $(cat requirements.txt)
+
+# Run code
 python notears-admm.py
