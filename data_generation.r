aa3d9312cd0bb7277aa7a30dae54eccf03500758
Niccolo-Rocchi <nik9991u@gmail.com>
2024-04-15 16:01:38 +0200

    Add graph and data generation file

---
+++ data_generation.r
@@ -0,0 +1,25 @@
+library(bnlearn)
+
+# Set seed
+set.seed(42)
+
+# Graph generation
+n.nodes <- 5
+nodes <- sprintf('%s', seq(1, n.nodes))
+G <- random.graph(nodes, method='melancon')
+
+# Parameters generation
+params <- list()
+for (x in nodes(G)) {
+  pars <- parents(G, x)
+  pars.coef <- rep(1, length(pars))
+  names(pars.coef) <- pars
+  params[[x]] <- list(coef=c('(Intercept)'=0, pars.coef), sd=1)
+}
+
+# Bayesian network fit
+bn <- custom.fit(G, params)
+
+# Data generation
+nsamples <- 1000
+rbn(bn, nsamples)
